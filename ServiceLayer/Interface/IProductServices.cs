96ed286e094eeb8caf9d7f2797be31cb32db2824
Vo HOA HIEP <vhoahiep@mantu.com>
2019-07-16 11:15:16 +0700

    add Service Layer

---
+++ ServiceLayer/Interface/IProductServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceLayer.Model;
+
+namespace ServiceLayer.Interface
+{
+    public interface IProductServices
+    {
+        IEnumerable<Product> GetAll();
+        void CreateOrUpdate(Product product);
+        void Delete(int productId);
+
+    }
+}
---
+++ ServiceLayer/Services/ProductServices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceLayer.Interface;
+using ServiceLayer.Model;
+
+namespace ServiceLayer.Services
+{
+    public class ProductServices :IProductServices
+    {
+        public IEnumerable<Product> GetAll()
+        {
+            return new List<Product>
+            {
+                new Product()
+                {
+                    Description = "Plastic Ball",
+                    Id = 1,
+                    Name = "BALL"
+                }
+            };
+        }
+
+        public void CreateOrUpdate(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
