0e00a19b813a6e1a35c9bbaec3036d87777e4b85
Vo HOA HIEP <vhoahiep@mantu.com>
2019-07-16 11:37:57 +0700

    update view

---
+++ cqrsmvc5/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ServiceLayer.Interface;
+
+namespace cqrsmvc5.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductServices _productServices;
+        public ProductController(IProductServices ProductService)
+        {
+            _productServices = ProductService;
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            var result = _productServices.GetAll();
+            return View(result);
+        }
+    }
+}
